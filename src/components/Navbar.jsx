0f2de3fc822245d84fdc84ab8bffe927fd449247
Aryan <aryan20072020@gmail.com>
2025-05-26 22:01:33 +0530

    cc

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
     </div>
     <div className="space-x-6 text-md font-medium">
       <a href="/" className="hover:text-yellow-400 transition">Home</a>
-      <a href="/add" className="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded-md transition">+ Add Blog</a>
+      <a href="https://bloghubv1.netlify.app/+/add" className="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded-md transition">+ Add Blog</a>
     </div>
   </div>
 </nav>
